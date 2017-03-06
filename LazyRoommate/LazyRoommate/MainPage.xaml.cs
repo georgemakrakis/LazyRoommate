817a4efcc3fec235624a1514ad45a69821c63e88
George Makrakis <George Makrakis>
2017-03-06 12:13:52 +0200

    Left menu changes

---
+++ LazyRoommate/LazyRoommate/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Xamarin.Forms;
+
+namespace LazyRoommate
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        private MobileServiceUser user { get; set; }
+       
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //making UWP back button visible
+            //App.currentView = SystemNavigationManager.GetForCurrentView();
+            //App.currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+
+            NavigationPage.SetHasNavigationBar(this, true);
+            NavigationPage.SetHasBackButton(this, true);
+
+            //hamburger.Text = "\xF0BA";
+
+
+        }
+
+        private void Button1_Clicked(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage();
+            IsPresented = false;
+        }
+        private void Button2_Clicked(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage();
+            IsPresented = false;
+        }
+        private void Button3_Clicked(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage();
+            IsPresented = false;
+        }
+    }
+}
