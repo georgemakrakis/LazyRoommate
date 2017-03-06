ea6556d3816bdfa1a1d246078eeb13d659c90a5c
Argiris Mouratidhs <Argiris Mouratidhs>
2017-03-06 11:14:53 +0200

    MasterDetailPage added (old version)

--- LazyRoommate/LazyRoommate/LazyRoommate/obj/Debug/LazyRoommate.MainPage.xaml.g.cs
+++ LazyRoommate/LazyRoommate/LazyRoommate/obj/Debug/LazyRoommate.MainPage.xaml.g.cs
@@ -14,7 +14,7 @@ namespace LazyRoommate {
     using Xamarin.Forms.Xaml;
     
     
-    public partial class MainPage : global::Xamarin.Forms.ContentPage {
+    public partial class MainPage : global::Xamarin.Forms.MasterDetailPage {
         
         [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
