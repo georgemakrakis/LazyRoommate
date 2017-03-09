79d7f22ab8decd7993d57a13849e42167a6a3c50
georgemakrakis <georgemakrakis88@gmail.com>
2017-03-09 12:33:44 +0200

    Social login images names changed

--- LazyRoommate/LazyRoommate.Droid/Resources/Resource.Designer.cs
+++ LazyRoommate/LazyRoommate.Droid/Resources/Resource.Designer.cs
@@ -2203,8 +2203,17 @@ namespace LazyRoommate.Droid
 			// aapt resource value: 0x7f0200a1
 			public const int mr_ic_play_light = 2130837665;
 			
+			// aapt resource value: 0x7f0200a5
+			public const int notification_template_icon_bg = 2130837669;
+			
 			// aapt resource value: 0x7f0200a2
-			public const int notification_template_icon_bg = 2130837666;
+			public const int social_connect_facebook = 2130837666;
+			
+			// aapt resource value: 0x7f0200a3
+			public const int social_connect_google = 2130837667;
+			
+			// aapt resource value: 0x7f0200a4
+			public const int social_connect_twitter = 2130837668;
 			
 			static Drawable()
 			{
--- LazyRoommate/LazyRoommate.Droid/obj/Debug/android/lazyroommate/droid/R.java
+++ LazyRoommate/LazyRoommate.Droid/obj/Debug/android/lazyroommate/droid/R.java
@@ -2560,7 +2560,10 @@ containing a value of this type.
         public static final int mr_ic_pause_light=0x7f02009f;
         public static final int mr_ic_play_dark=0x7f0200a0;
         public static final int mr_ic_play_light=0x7f0200a1;
-        public static final int notification_template_icon_bg=0x7f0200a2;
+        public static final int notification_template_icon_bg=0x7f0200a5;
+        public static final int social_connect_facebook=0x7f0200a2;
+        public static final int social_connect_google=0x7f0200a3;
+        public static final int social_connect_twitter=0x7f0200a4;
     }
     public static final class id {
         public static final int action0=0x7f07008b;
