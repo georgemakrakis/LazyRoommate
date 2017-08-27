6df6270b99693c7799e9e7716117de4af46be2fb
georgemakrakis <georgemakrakis88@gmail.com>
2017-08-28 01:32:11 +0300

    Started user deletion from app

--- LazyRoommateLibrary/MainPage.xaml.cs
+++ LazyRoommateLibrary/MainPage.xaml.cs
@@ -226,7 +226,7 @@ namespace LazyRoommate
             App.Email = string.Empty;
             App.ProfileImage = string.Empty;
             App.ProfileName = string.Empty;          
-            await Navigation.PushAsync(new LoginPage(), true);
+            await Navigation.PushModalAsync(new LoginPage(), true);
 
             //This just came up just for security-reverse engineering reasons i think...
             Navigation.RemovePage(this);
@@ -235,6 +235,11 @@ namespace LazyRoommate
         private void LoadingList(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
 
+        }
+
+        private void Settings_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new SettingsPage());
         }
     }
 }
